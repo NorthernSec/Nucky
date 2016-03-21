83f2b73e1a2e8f52fd3c0bcf0ecaee2cbb8d3d2b
Robin <info@lesmarques.se>
2016-03-21 17:35:40 +0100

    Nucky got a memory upgrade.

--- src/knowledge.h
+++ src/knowledge.h
@@ -7,6 +7,14 @@
 struct the_mind{
 	const char	*answer;
 } knowledge[] = {
+	{"That's interesting, keep going..."},
+	{"Mhm."},
+	{"I see, is it supposed to be like that?"},
+	{"Perhaps your going about this from the wrong angle."},
+	{"Can you be a little bit more specific?"},
+	{"Are you sure that's really a bug?"},
+	{"Do you know in which file the error occurs?"},
+	{"Perhaps it's working as intended?"},
 	{"Have you tried with different input?"},
 	{"You should add breakpoints and compile with debugging."},
 	{"Have you checked for speed up loops?"},
